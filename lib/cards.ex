e47703a4be135177804bb0e43323707db1c31782
unknown <tqk19112001@gmail.com>
2022-02-20 16:17:22 +0700

    Warmup and Pattern matching

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,48 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
+  @spec create_deck :: list
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    cards = for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+    List.flatten(cards)
   end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  #pattern matching
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "That file does not exist"
+      # name with underscore in the head => we know but we dont care it
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  #pipe operator
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+
+  end
+
+
 end
